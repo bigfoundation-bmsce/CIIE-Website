9f787e173976c3be8dc26f8056553d9585499b28
Sudarshana V Sharma <svsccodes@gmail.com>
2024-01-12 22:06:01 +0530

    update react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,14 +4,19 @@ import Register from './pages/Register/Register'
 import Walkthrough from './pages/Walkthrough/Walkthrough'
 import LaunchPad from './pages/LaunchPad/LaunchPad';
 import Team  from './pages/Team/Team';
+import {BrowserRouter,Routes,Route} from "react-router-dom"
 export default function App() {
   return (
     <div className="App">
-      <Home />
-      <Walkthrough/>
-      <Register/>
-      <LaunchPad/>
-       <Team/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>} exact />
+          <Route path='/team' element={<Team/>}/>
+          <Route path='/walkthrough' element={<Walkthrough/>}/>
+          <Route path='/LaunchPad' element={<LaunchPad/>}/>
+          <Route path='/register' element={<Register/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
