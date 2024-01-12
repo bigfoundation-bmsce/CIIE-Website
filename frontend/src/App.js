71d53705b6d9508f9983ccc98f1948815fece566
Sudarshana V Sharma <svsccodes@gmail.com>
2024-01-12 19:37:54 +0530

    Team export resolve

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 import Register from './pages/Register/Register'
 import Walkthrough from './pages/Walkthrough/Walkthrough'
 import LaunchPad from './pages/LaunchPad/LaunchPad';
-import { Team } from './pages/Team/Team';
+import Team  from './pages/Team/Team';
 export default function App() {
   return (
     <div className="App">
