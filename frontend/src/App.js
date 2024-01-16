3704609cefa1776e46c2c84ebc686c0ef45c315f
J-ON-AS <118046576+J-ON-AS@users.noreply.github.com>
2024-01-16 19:38:42 +0530

    Routing successfull upto home , contact , team

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,24 +5,25 @@ import Walkthrough from './pages/Walkthrough/Walkthrough'
 import LaunchPad from './pages/LaunchPad/LaunchPad';
 import Team  from './pages/Team/Team';
 import {BrowserRouter,Routes,Route} from "react-router-dom"
-
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+// import Devwing from './pages/Devwing/Devwing';
 export default function App() {
   return (
     <div className="App">
-      {/* <Home /> */}
-      {/* <Walkthrough/> */}
-      {/* <Register/> */}
-      {/* <LaunchPad/> */}
-      <Team/>
-      {/* <BrowserRouter> */}
-        {/* <Routes> */}
-          {/* <Route path='/' element={<Home/>} exact /> */}
-          {/* <Route path='/team' element={<Team/>}/> */}
-          {/* <Route path='/walkthrough' element={<Walkthrough/>}/> */}
+      <Header/>
+        <Routes>
+
+          <Route path='/' element={<Home/>} exact />
+          <Route path='/team' element={<Team/>}/>
+          {/* <Route path='/contact' element={<Contact/>}/> */}
           {/* <Route path='/LaunchPad' element={<LaunchPad/>}/> */}
-          {/* <Route path='/register' element={<Register/>}/> */}
-        {/* </Routes> */}
-       {/* </BrowserRouter> */}
+          <Route path='/register' element={<Register/>}/>
+          {/* <Route path='/Devwing' element={<Devwing/>}/> */}
+
+        </Routes>
+        <Footer/>
     </div>
+
   )
 }
